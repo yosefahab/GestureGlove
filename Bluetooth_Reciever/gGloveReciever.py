73b03a3aa855de43b0f5407623724f20c7afbb6f
Youssef <61936188+yosefahab@users.noreply.github.com>
2021-12-09 14:24:43 +0200

    added script auto terminate capability

--- Bluetooth_Reciever/gGloveReciever.py
+++ Bluetooth_Reciever/gGloveReciever.py
@@ -1,13 +1,14 @@
 import serial
 import pyautogui
 import time
+import sys
 
 gestures = { "idle": None,
              "up-swipe": "volumeup",
              "down-swipe": "volumedown",
              "right-tilt": "nexttrack",
              "left-tilt": "prevtrack",
-             "circular": "playpause",}
+             "circular": "playpause"}
 
 def retrieveData(ser):
     return ser.readline().decode("ascii")
@@ -16,19 +17,34 @@ prevKey = "idle"
 
 def performAction(gest):
     global prevKey
+    # prevents double actions (essentially canceling or doubling previous action)
     if prevKey != gest and gest !="idle":
         pyautogui.press(gestures.get(gest))
-        time.sleep(.75)
     prevKey = gest
     return
 
-def main():
-    # change COM accordingly
-    ser = serial.Serial("COM5",115200)
-    while True:
-        gest = retrieveData(ser).strip()
-        print("Read:",gest,"Action:",gestures.get(gest))
-        performAction(gest)
+def main(argv):
+    try:
+        # COM passed in program argv
+        com = argv[1]
+        print("Searching for Device on port: %s..." %com)
+        ser = serial.Serial(com,115200,timeout=7)
+    except:
+        print("Device not Connected! Terminating...")
+        exit(1)
+    else:
+        print("Listening to device on port: %s!" %com)
+        while True:
+            time.sleep(.75)
+            try:                    
+                gest = retrieveData(ser).strip()
+                # remove this print if uninterested in output
+                print("Read:",gest,"Action:",gestures.get(gest))
+                performAction(gest)
+            except:
+                print("Device Disconnected! Terminating...")
+                ser.close()
+                exit(1)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
